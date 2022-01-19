3037c32ed754969833288c428ae2756c6bd46cbf
Samip Timilsina <timi.samip@gmail.com>
2022-01-19 20:01:17 +0545

    adds reset functioning after certain limit

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-let firstNumber=Math.floor(Math.random()*13+1);
-let secondNumber=Math.floor(Math.random()*13+1);
-let sumNumber=firstNumber+secondNumber;
+let firstNumber=0;
+let secondNumber=0;
+let sumNumber=0;
 
-function ifchecking(){
+function startgame(){
+    initialMessage();
+    firstNumber=Math.floor(Math.random()*13+1);
+    secondNumber=Math.floor(Math.random()*13+1);
+    sumNumber=firstNumber+secondNumber;
+    document.querySelector(".cards").innerHTML+=firstNumber+" "+secondNumber;
+    document.querySelector(".sum").innerHTML+=sumNumber;
+    ifchecking(sumNumber);
+}
+
+function ifchecking(sumNumber){
     if (sumNumber<21) {
         document.querySelector(".status").innerHTML="One more card to try your luck?"
     } else if (sumNumber==21) {
         document.querySelector(".status").innerHTML="You've got blackjack!"
+        reset();
     } else {
         document.querySelector(".status").innerHTML="Sorry, you've failed"
+        reset();
     }
 }
 
 
-function startgame(){
-    document.querySelector(".cards").innerHTML+=firstNumber+" "+secondNumber;
-    document.querySelector(".sum").innerHTML+=sumNumber;
-    ifchecking();
+function reset(){
+    setTimeout(() => {
+        // firstNumber=0;
+        // secondNumber=0;
+        // thirdNumber=0;
+        sumNumber=0;
+        initialMessage();
+    }, 3000);
+}
+
+function initialMessage(){
+        document.querySelector(".status").innerHTML="Want to play a round?";
+        document.querySelector(".cards").innerHTML="Cards: ";
+        document.querySelector(".sum").innerHTML="Sum: ";
 }
 
 function newcard(){
+    document.querySelector(".sum").innerHTML="Sum: "
     let thirdNumber=Math.floor(Math.random()*13+1);
+    console.log(thirdNumber)
+    console.log(sumNumber)
     sumNumber+=thirdNumber;
-    ifchecking();
+    document.querySelector(".cards").innerHTML+=" "+thirdNumber;
+    document.querySelector(".sum").innerHTML+=sumNumber;
+    ifchecking(sumNumber);
 }
